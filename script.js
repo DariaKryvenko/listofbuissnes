74f26fd314bc26761218ca27c158e6b60bfeb67f
kostyazem <129662297+kostyazem@users.noreply.github.com>
2025-05-18 22:06:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-let add= document.querySelector('.add')
-let input = document.querySelector(".input")
-add.addEventListener('click',function(){
-    let text=input.value
-    let container= document.querySelector('.container')
-    container.innerHTML+=` 
-    <div class="element">
-    <input type="checkbox">
-    <span>${text}</span>
-    <button class="delete-btn">удалить</button>
-</div>  
-// получить все кнопки удалить    `
-})
-const deleteButtons = document.querySelectorAll('.delete-btn');
+let add = document.querySelector('.add');
+let input = document.querySelector(".input");
+let container = document.querySelector('.container');
 
-deleteButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    button.parentElement.remove();
-  });
+add.addEventListener('click', function() {
+    let text = input.value;
+    container.innerHTML += `
+    <div class="element">
+        <input type="checkbox" class='save'>
+        <span class='text'>${text}</span>
+        <button class="delete-btn">удалить</button>
+    </div>
+    `;
+
+    // Находим ВСЕ кнопки удаления
+    let deleteButtons = container.querySelectorAll('.delete-btn');
+    deleteButtons.forEach(btn => {
+        btn.onclick = function() {
+            btn.parentElement.remove();
+        };
+    });
 });
